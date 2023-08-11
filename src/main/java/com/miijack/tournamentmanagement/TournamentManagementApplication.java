ab51866b1dfbba5545928a453315eeb8d96216ef
Mii <32428593+MiiJack@users.noreply.github.com>
2023-08-11 12:53:43 +0300

    init : "initializing project"

---
+++ src/main/java/com/miijack/tournamentmanagement/TournamentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.miijack.tournamentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TournamentManagementApplication.class, args);
+    }
+
+}
